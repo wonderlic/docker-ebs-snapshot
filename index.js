ce59d1ae6b4e9ea84a15a2e13abd22bf5a446d23
Jamieson.Smith <Jamie.smith@wonderlic.com>
2015-11-25 12:46:15 -0600

    Wired up creating snapshots and creating tags. Added command line args. Added debug

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+var _ = require('lodash');
 var AWS = require('aws-sdk');
+var commandLineArgs = require('command-line-args');
+var debug = require('debug');
+
+//---Debug Options
+var optionsDebug = debug('options');
+var purgeDebug = debug('purge');
+var tagDebug = debug('tag');
+var snapShotDebug = debug('snapShot');
+
+var cli = commandLineArgs([
+  {name: 'purge', alias: 'p', type: Boolean, defaultOption: false},
+  {name: 'snapShotTimerTag', alias: 's', type: String}
+]);
 
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -6,9 +20,11 @@ AWS.config.update({
   region: process.env.AWS_DEFAULT_REGION
 });
 
-var ec2 = new AWS.EC2();
+var ec2 = new AWS.EC2({region: 'us-east-1'});
 
 function _purgeExpiredSnapshots(cb) {
+  purgeDebug('Starting Purge');
+
   var params = {
     DryRun: false,
     Filters: [
@@ -30,9 +46,7 @@ function _purgeExpiredSnapshots(cb) {
             };
 
             ec2.deleteSnapshot(param, function(err, data) {
-              if (err) {
-                console.log('ERROR:', snapShot.SnapshotId);
-              }
+              if (err) { return cb(err); }
               else {
                 console.log('Deleted Snapshot with ID: ', snapShot.SnapshotId);
               }
@@ -41,6 +55,70 @@ function _purgeExpiredSnapshots(cb) {
           timeout = timeout + 125;//throttle the deletes so AWS doesn't error on over limit
         }
       });
+    });
+  });
+}
+
+function _createTag(resources, tagName, tagValue, cb) {
+  tagDebug('Creating Tag');
+
+  if (!_.isArray(resources)) {
+    tagDebug('Converting to array');
+    var resourcesString = resources;
+    resources = [];
+    resources[0] = resourcesString;
+  }
+
+  tagDebug('resources', resources);
+  tagDebug('tagName', tagName);
+  tagDebug('tagValue', tagValue);
+
+  var params = {
+    Resources: resources,
+    Tags: [
+      {
+        Key: tagName,
+        Value: tagValue
+      }
+    ],
+    DryRun: false
+  };
+
+  tagDebug('params', params);
+
+  ec2.createTags(params, function(err, tagData) {
+    if (err) {
+      tagDebug('err', err);
+      return cb(err);
+    }
+    tagDebug('tagData', tagData);
+  });
+}
+
+function _createSnapshots(snapShotTimerTag, cb) {
+  snapShotDebug('Creating Snapshot');
+  var modEpoch = (new Date()).getTime() / 1000;
+  var params = {
+    DryRun: false,
+    Filters: [
+      {Name: 'tag-key', Values: [snapShotTimerTag]}
+    ]
+  };
+
+  ec2.describeVolumes(params, function(err, data) {
+    if (err) { return cb(err); }
+    _.each(data.Volumes, function(volume) {
+      var params = {
+        VolumeId: volume.VolumeId,
+        Description: snapShotTimerTag + ' - ' + modEpoch,
+        DryRun: false
+      };
+      ec2.createSnapshot(params, function(err, snapShotData) {
+        if (err) {return cb(err)}
+        snapShotDebug('ReturnData', snapShotData);
+        _createTag(snapShotData.SnapshotId, 'Name', volume.VolumeId)
+      });
+
     });
   });
 }
@@ -57,9 +135,20 @@ function _handleErrorAndExit(err) {
 // Main processing logic...
 
 function _start() {
-  _purgeExpiredSnapshots(function(err) {
-    if (err) {return _handleError(err); }
-  })
+  var options = cli.parse();
+  optionsDebug('options', options);
+
+  if (options.snapShotTimerTag) {
+    _createSnapshots(options.snapShotTimerTag, function(err) {
+      if (err) {return _handleError(err); }
+    });
+  }
+
+  if (options.purge) {
+    _purgeExpiredSnapshots(function(err) {
+      if (err) {return _handleError(err); }
+    });
+  }
 }
 
 _start();
