484924fa95cac73acd28595d7fe31048d188e423
Stephen Erisman <stephen.erisman@wonderlic.com>
2020-03-16 13:10:32 -0500

    minor cleanup/refactoring

--- index.js
+++ index.js
@@ -33,16 +33,28 @@ function sleep(ms) {
   });
 }
 
+function getTimestamp() {
+  return Math.floor((new Date()).getTime() / 1000);
+}
+
+function getTagValue(tags, name) {
+  const tag = _.find(tags, {key: name});
+  if (tag) {
+    return tag.Value;
+  }
+}
+
 async function _purgeExpiredSnapshots(throttleDelay) {
   console.log('Purging expired snapshots');
 
-  var modEpoch = Math.floor((new Date()).getTime() / 1000);
+  const timestamp = getTimestamp();
 
   const snapshots = await ec2.listSnapshotsWithTag('PurgeAllow');
   for (const snapshot of snapshots) {
 
-    const purgeAfterFE = _getTagValue(snapshot.Tags, 'PurgeAfterFE');
-    if (purgeAfterFE && purgeAfterFE < modEpoch ) {
+    const purgeAllow = getTagValue(snapshot.Tags, 'PurgeAllow');
+    const purgeAfterFE = getTagValue(snapshot.Tags, 'PurgeAfterFE');
+    if (purgeAllow === 'true' && purgeAfterFE && timestamp > parseInt(purgeAfterFE)) {
       await ec2.deleteSnapshot(snapshot.SnapshotId);
       console.log('Deleted expired SnapshotId: %s', snapshot.SnapshotId);
 
@@ -70,34 +82,27 @@ async function _applyTagsToSnapshot(snapshotId, volumeName, purgeAfterFE) {
 async function _createSnapshots(snapshotTag, purgeAfter, copyTo) {
   console.log('Creating snapshots for volumes with tag: %s', snapshotTag);
 
-  const modEpoch = Math.floor((new Date()).getTime() / 1000);
+  const timestamp = getTimestamp();
 
   let purgeAfterFE = 0;
   if (purgeAfter > 0) {
-    purgeAfterFE = modEpoch + (purgeAfter * 60 * 60);
+    purgeAfterFE = timestamp + (purgeAfter * 60 * 60);
     console.log('Allow snapshots to be purged after %d hours (%d)', purgeAfter, purgeAfterFE);
   }
 
   const volumes = await ec2.listVolumesWithTag(snapshotTag);
   for (const volume of volumes) {
 
-    const volumeName = _getTagValue(volume.Tags, 'Name');
-    const snapshot = await ec2.createSnapshot(volume.VolumeId, `${snapshotTag} - ${modEpoch}`);
-    await _applyTagsToShapshot(snapshot.SnapshotId, volumeName, purgeAfterFE);
+    const volumeName = getTagValue(volume.Tags, 'Name');
+    const snapshot = await ec2.createSnapshot(volume.VolumeId, `${snapshotTag} - ${timestamp}`);
+    await _applyTagsToSnapshot(snapshot.SnapshotId, volumeName, purgeAfterFE);
     console.log('Created snapshot for VolumeId: %s SnapshotId: %s', volumeName ? volume.VolumeId + ' (' + volumeName + ')' : volume.VolumeId, snapshot.SnapshotId);
 
     if (copyTo) {
-      const clonedSnapshot = await ec2.copySnapshot(snapshot.SnapshotId, process.env.AWS_DEFAULT_REGION, copyTo, `CLONE: ${snapshotTag} - ${modEpoch}`);
-      await _applyTagsToShapshot(snapshot.SnapshotId, volumeName, purgeAfterFE);
+      const clonedSnapshot = await ec2.copySnapshot(snapshot.SnapshotId, process.env.AWS_DEFAULT_REGION, copyTo, `CLONE: ${snapshotTag} - ${timestamp}`);
+      await _applyTagsToSnapshot(snapshot.SnapshotId, volumeName, purgeAfterFE);
       console.log('Cloned snapshot for VolumeId: %s SnapshotId: %s', volumeName ? volume.VolumeId + ' (' + volumeName + ')' : volume.VolumeId, clonedSnapshot.SnapshotId);
     }
-  }
-}
-
-function _getTagValue(tags, name) {
-  const tag = _.find(tags, {key: name});
-  if (tag) {
-    return tag.Value;
   }
 }
 
